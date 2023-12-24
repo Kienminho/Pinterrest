94900b07967bf9572c8028aa5df66b73f115120d
Trần Hợp Kiên <hopkien1609@gmail.com>
2023-12-24 10:03:20 +0700

    init source

---
+++ back-end/app.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const session = require("express-session");
+const cookieParser = require("cookie-parser");
+require("dotenv").config();
+const app = express();
+
+//middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(
+  session({
+    secret: "my-key",
+    resave: true,
+    saveUninitialized: true,
+    cookie: {
+      secure: false,
+      maxAge: 60 * 60 * 24 * 1000,
+    },
+  })
+);
+app.use(express.static("public"));
+app.use(cookieParser());
+app.listen(process.env.PORT, () => {
+  console.log("Server is running on: http://localhost:" + process.env.PORT);
+});
