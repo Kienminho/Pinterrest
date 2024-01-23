81dec983b6255bc3d81893de98ce11a85071689e
Trần Hợp Kiên <hopkien1609@gmail.com>
2024-01-23 16:03:19 +0700

    open all cros

--- back-end/app.js
+++ back-end/app.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
+const cors = require("cors");
 require("dotenv").config();
 const app = express();
 
 //middleware
+app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static("public"));
