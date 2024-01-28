0d497fc07e5a29b64c7eec714a1f81426c78ef14
Trần Hợp Kiên <hopkien1609@gmail.com>
2024-01-28 17:18:49 +0700

    add controller, router, model Comment

---
+++ back-end/controller/PostController.js
@@ -0,0 +1,72 @@
+const Utils = require("../common/Utils");
+const _Post = require("../model/Post");
+
+const HandleGetPostsByUser = async (req, res) => {
+  try {
+    const posts = await _Post.find({ Created: req.user.id });
+    res.json(Utils.createSuccessResponseModel(posts.length, posts));
+  } catch (error) {
+    console.log("PostController - HandleGetPostsByUser: " + error.message);
+    return res.status(500).json(Utils.createErrorResponseModel(error.message));
+  }
+};
+
+const HandleCreatePost = async (req, res) => {
+  try {
+    const { Title, Description, Attachment } = req.body;
+    //validate
+    if (!Title || !Description || !Attachment) {
+      return res
+        .status(400)
+        .json(
+          Utils.createErrorResponseModel("Vui lòng nhập đầy đủ thông tin!")
+        );
+    }
+
+    const newPost = req.body;
+    newPost.Created = req.user.id;
+    const post = new _Post(newPost);
+    await post.save();
+    res.json(Utils.createSuccessResponseModel(0, true));
+  } catch (error) {
+    console.log("PostController - HandleCreatePost: " + error.message);
+    return res.status(500).json(Utils.createErrorResponseModel(error.message));
+  }
+};
+
+const HandleUpdatePost = async (req, res) => {
+  try {
+    const { PostId, Title, Description, IsComment = true } = req.body;
+    //validate
+    if (!Title || !Description) {
+      return res
+        .status(400)
+        .json(
+          Utils.createErrorResponseModel("Vui lòng nhập đầy đủ thông tin!")
+        );
+    }
+
+    const existPost = await _Post.findById(PostId);
+    if (!existPost) {
+      return res
+        .status(400)
+        .json(Utils.createErrorResponseModel("Bài đăng không tồn tại!"));
+    }
+    existPost.Title = Title;
+    existPost.Description = Description;
+    existPost.IsComment = IsComment;
+    existPost.Updated = req.user.id;
+    existPost.UpdateAt = Date.now();
+    await existPost.save();
+    res.json(Utils.createSuccessResponseModel(0, true));
+  } catch (error) {
+    console.log("PostController - HandleCreatePost: " + error.message);
+    return res.status(500).json(Utils.createErrorResponseModel(error.message));
+  }
+};
+
+module.exports = {
+  HandleGetPostsByUser: HandleGetPostsByUser,
+  HandleCreatePost: HandleCreatePost,
+  HandleUpdatePost: HandleUpdatePost,
+};
