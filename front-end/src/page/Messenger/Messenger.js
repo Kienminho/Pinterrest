e24ca1e5aa947ac6f427f816abd9997e3b63701c
Trần Hợp Kiên <hopkien1609@gmail.com>
2024-03-13 00:09:30 +0700

    Update socket URL in Messenger.js

--- front-end/src/page/Messenger/Messenger.js
+++ front-end/src/page/Messenger/Messenger.js
@@ -25,7 +25,7 @@ const Messenger = () => {
   const messageRef = useRef(null)
 
   useEffect(() => {
-    setSocket(io(`https://api-pinterrest.up.railway.app:6531`))
+    setSocket(io(`https://api-pinterrest.up.railway.app:443`))
   }, [])
 
   useEffect(() => {
