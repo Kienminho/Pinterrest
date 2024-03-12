d6b481e00fcb2d60dfee0b3eeed8882554c5cf71
johnpham <pltnhan311@gmail.com>
2024-03-12 23:26:28 +0700

    fix url socket

--- front-end/src/page/Messenger/Messenger.js
+++ front-end/src/page/Messenger/Messenger.js
@@ -25,7 +25,7 @@ const Messenger = () => {
   const messageRef = useRef(null)
 
   useEffect(() => {
-    setSocket(io(`https://api-pinterrest.up.railway.app/${process.env.PORT_SOCKET}`))
+    setSocket(io(`https://api-pinterrest.up.railway.app:6531`))
   }, [])
 
   useEffect(() => {
